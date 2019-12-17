ae807f77199f8091bc8d22547de57200ccb4166d
Pasquale Paudice <pasquale.paudice10@gmail.com>
2019-12-17 13:09:57 +0100

    // Milestone 2
    aggiunta valutazione in stelle
    aggiunta bandiera inglese e bandiera americana

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ $(document).ready(function(){
 
 
 
+$('#cerca-film').keypress(function(event){
+
+    if (event.which == 13) {
+
+        ricerca();
+
+    }
+
+});
+
+$('#button-cerca-film').click(ricerca);
 
 
 
 
-
-
-
-
-
-    ricerca();
 
 
 
@@ -26,50 +31,94 @@ $(document).ready(function(){
 
 function ricerca(){
 
-    $('#button-cerca-film').click(function(){
+
 
         var film_cercato = $('#cerca-film').val();
 
+         $('#cerca-film').val('');
+
+        if (film_cercato.length != 0) {
+            $.ajax({
+                'url':'https://api.themoviedb.org/3/search/movie',
+                'data': {
+                    'api_key':'f3534c353eab17db2456e44f0cf8e1b0',
+                    'query':film_cercato,
+                    'language':'it-IT'
+                },
+                'methods':'get',
+                'success':function(data){
+                    $('.card').remove();
+                    var template_card = $('#template-card').html();
+                    var template_function = Handlebars.compile(template_card);
 
 
-        $.ajax({
-            'url':'https://api.themoviedb.org/3/search/movie',
-            'data': {
-                'api_key':'f3534c353eab17db2456e44f0cf8e1b0',
-                'query':film_cercato,
-                'language':'it-IT'
-            },
-            'methods':'get',
-            'success':function(data){
-                $('.card').hide();
-                var template_card = $('#template-card').html();
-                var template_function = Handlebars.compile(template_card);
+                    var film = data.results;
 
-                var film = data.results;
-                for (var i = 0; i < film.length; i++) {
-                    var film_corrente=film[i];
-
-                    var compilazione = {
-
-                        titolo : film_corrente.title,
-                        titolo_originale : film_corrente.original_title,
-                        lingua : film_corrente.language,
-                        voto : film_corrente.vote_average,
-                        copertina : 'https://image.tmdb.org/t/p/w500' + film_corrente.poster_path,
-
-                    }
-
-                        $('.container').append(template_function(compilazione));
+                    for (var i = 0; i < film.length; i++) {
 
 
+                        var film_corrente=film[i];
+
+                        var stelle = Math.ceil(film_corrente.vote_average / 2);
+
+                        var votazione ='';
+
+                        console.log(stelle);
+
+                        for (var j = 0; j < stelle; j++) {
+                            votazione += '<i class="fas fa-star"></i>';
+                            console.log(votazione);
+                        };
+
+
+
+                        var stelle_vuote = 5 - stelle;
+                        var v = 0;
+                        while (v != stelle_vuote) {
+                            votazione += '<i class="far fa-star"></i>';
+                            v++;
+                        }
+
+
+
+
+
+                        if (film_corrente.original_language == 'en') {
+                            film_corrente.original_language = '<img class="flag" src="flag-en.png" alt="">';
+                        }
+
+                        if (film_corrente.original_language == 'it') {
+                            film_corrente.original_language = '<img class="flag" src="flag-it.png" alt="">';
+                        }
+
+
+
+
+                        var compilazione = {
+
+                            titolo : film_corrente.title,
+                            titolo_originale : film_corrente.original_title,
+                            lingua: film_corrente.original_language,
+                            voto : film_corrente.vote_average,
+                            copertina : 'https://image.tmdb.org/t/p/w500' + film_corrente.poster_path,
+                            stelle : votazione
+
+                        }
+
+                            $('.container').append(template_function(compilazione));
+
+
+                    };
+                },
+                'error':function(data){
+                    alert('error');
                 }
-            },
-            'error':function(data){
-                alert('error');
-            }
 
 
-        });
+            });
+        }
 
-    });
+
+
+
 };
